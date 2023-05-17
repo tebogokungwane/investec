e331c3ab57e47e1f7a9a8efacbdd868f9bb82287
Lebo <tjkungwane@gmail.com>
2023-05-17 20:41:33 +0200

    adding negative scenarios

--- investec/src/test/java/co/za/programming/tech/invest/AddressServiceTest.java
+++ investec/src/test/java/co/za/programming/tech/invest/AddressServiceTest.java
@@ -1,5 +1,6 @@
 package co.za.programming.tech.invest;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
@@ -83,4 +84,39 @@ public class AddressServiceTest {
         assertThat(prettyPrintAddress, is("Physical Address: Address 1 - City 1 - Eastern Cape - 1234 - South Africa"));
 
     }
+
+    @Test
+    public void testPrettyPrintAddress_NegativeScenario(){
+        address = new Address();
+        AddressPrinter addressPrinter = new AddressPrinter();
+
+        address.setId("1");
+        Type type = new Type();
+        type.setCode("1");
+        type.setName("Physical Address");
+        address.setType(type);
+
+        AddressLineDetail addressLineDetail = new AddressLineDetail();
+        addressLineDetail.setLine1("Address 1");
+        addressLineDetail.setLine2("Line 2");
+        address.setAddressLineDetail(addressLineDetail);
+
+        ProvinceOrState provinceOrState = new ProvinceOrState();
+        provinceOrState.setCode("0");
+        provinceOrState.setName("Cape Town");
+        address.setProvinceOrState(provinceOrState);
+        address.setCityOrTown("City 1");
+
+        Country country = new Country();
+        country.setCode("ZA");
+        country.setName("South Africa");
+        address.setCountry(country);
+        address.setPostalCode("1234");
+        address.setLastUpdated("2015-06-21T00:00:00.000Z");
+
+
+        String prettyPrintAddress = addressPrinter.prettyPrintAddress(address);
+
+        assertThat(prettyPrintAddress, Matchers.not("Physical Address: Address 1 - City 1 - Eastern Cape - 1234 - South Africa"));
+
 }
--- investec/src/test/java/co/za/programming/tech/invest/assignment1/CommonFactorTest.java
+++ investec/src/test/java/co/za/programming/tech/invest/assignment1/CommonFactorTest.java
@@ -1,17 +1,35 @@
 package co.za.programming.tech.invest.assignment1;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CommonFactorTest {
+
+    private CommonFactor commonFactor;
+
+    @Before
+    public void setup(){
+        commonFactor = new CommonFactor();
+
+    }
+
     @Test
     public void testHighestCommonFactor() {
-        CommonFactor hcf = new CommonFactor();
         int[] input = {12, 16, 20};
         int expected = 4;
-        int actual = hcf.highestCommonFactor(input);
+        int actual = commonFactor.highestCommonFactor(input);
         assertThat(actual, Matchers.equalTo(expected));
+    }
+
+    @Test
+    public void testHighestCommonFactor_NegativeScenario(){
+        int[] input = {12,16,20};
+        int expected = 6;
+        int actual = commonFactor.highestCommonFactor(input);
+        assertThat(actual, Matchers.not(Matchers.equalTo(expected)));
+
     }
 }
